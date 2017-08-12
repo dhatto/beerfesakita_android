0d4d955e1be04db2e1ebaf347c5266ca6a2d131b
Daigoh Hattori <ramenplease@gmail.com>
2017-08-12 23:49:23 +0900

    バグ。ActivityResultのコードが等しいActivityが複数あると、画面が表示されない

--- app/src/main/java/com/mashupnext/beerfesakita/ActivityCode.java
+++ app/src/main/java/com/mashupnext/beerfesakita/ActivityCode.java
@@ -2,9 +2,9 @@ package com.mashupnext.beerfesakita;
 
 public class ActivityCode {
 
-	public final static int REQUEST_PICK_EXISTING_PHOTO = 2;
-	public final static int REQUEST_CAMERA = 1;
-	public final static int RESULT_CANCEL = 2;
+	public final static int REQUEST_PICK_EXISTING_PHOTO = 1;
+	public final static int REQUEST_CAMERA = 2;
+	public final static int RESULT_CANCEL = 3;
 }
 
 
